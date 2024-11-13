adafcadc9c6999bb8c6158350d6e647d516f5166
zeltic <benjaminbasauri@gmail.com>
2024-11-13 20:01:41 -0300

    updated load_infra

--- Api infraestructura/load_infra.py
+++ Api infraestructura/load_infra.py
@@ -1,16 +1,16 @@
 import osmnx as ox
 import geopandas as gpd
 
-# Define el área geográfica de Santiago
-place = "Santiago, Chile"
+# Define el área geográfica como la región metropolitana de Santiago
+place = "Región Metropolitana de Santiago, Chile"
 
-# Cargar las calles de la zona
+# Descargar todas las calles de la región
 G = ox.graph_from_place(place, network_type="drive")
 
-# Obtener las intersecciones como puntos GeoDataFrame
+# Convertir el grafo a GeoDataFrames de nodos y aristas
 nodes, edges = ox.graph_to_gdfs(G, nodes=True, edges=True)
 
-# Filtrar solo las intersecciones
+# Filtrar solo las intersecciones (nodos donde se cruzan más de 2 calles)
 intersections = nodes[nodes['street_count'] > 2]
 
 # Exportar a GeoJSON
